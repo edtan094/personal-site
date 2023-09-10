d31830cd71d13737edb339d36f5e8bc3e1d010e3
etan094 <etan094@gmail.com>
2023-09-09 19:12:29 -0700

    Using test-library and jest to run tests.

--- dom.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "bun:test";
-import { JSDOM } from "jsdom";
-
-test("dom test", () => {
-  const h2 = document.querySelector("h2");
-  console.log("h2", h2);
-  expect(h2?.innerText).toEqual("Hello, my name is Edwin welcome to my site");
-});
