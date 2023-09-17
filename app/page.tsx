5c06d70b991f877480e2361ac25eb301a604c98a
etan094 <etan094@gmail.com>
2023-09-16 17:33:08 -0700

    Using graphql and experimental apollo that can be used with next js 13.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-export default function Home() {
+import { getClient } from "@/lib/client";
+
+import { gql } from "@apollo/client";
+
+const query = gql`
+  query Now {
+    now(id: "1")
+  }
+`;
+
+export default async function Home() {
+  const { data } = await getClient().query({
+    query,
+    context: {
+      fetchOptions: {
+        next: { revalidate: 5 },
+      },
+    },
+  });
+  console.log("data", data);
   return (
     <>
       <main>
         <div className="flex justify-center">
           <h2>Hello, my name is Edwin welcome to my site</h2>
         </div>
+        <div>The time is {data.now}</div>
       </main>
     </>
   );
