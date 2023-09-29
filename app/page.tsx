da08ceac55c3ccb8d7c96f59149ac338248fb664
etan094 <etan094@gmail.com>
2023-09-29 11:17:15 -0700

    Completed query for project.

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,9 @@
-import { getClient } from "@/lib/client";
-
-import { gql } from "@apollo/client";
-
-const query = gql`
-  query MyQuery {
-    projects {
-      id
-      project_description
-      project_name
-    }
-  }
-`;
-
-export const revalidate = 5;
-
-interface IProject {
-  project_name: string;
-}
-
 export default async function Home() {
-  const { data } = await getClient().query({
-    query,
-    fetchPolicy: "no-cache",
-  });
-  console.log("data", data);
   return (
     <>
       <main>
         <div className="flex justify-center">
           <h2>Hello, my name is Edwin welcome to my site</h2>
-        </div>
-        <div>
-          Here is my project.{" "}
-          {data.projects.map((project: IProject) => project.project_name)}
         </div>
       </main>
     </>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,52 @@
-import React from "react";
+import { getClient } from "@/lib/client";
 
-export default function Project() {
-  return <div>Project</div>;
+import { gql } from "@apollo/client";
+
+const query = gql`
+  query MyQuery {
+    projects {
+      created_at
+      id
+      project_name
+      description
+      url_link
+      github_url_link
+    }
+  }
+`;
+
+export const revalidate = 5;
+
+interface IProject {
+  id: number;
+  project_name: string;
+  description: string;
+  url_link: string;
+  github_url_link: string;
+}
+
+export default async function Project() {
+  const { data } = await getClient().query({
+    query,
+    fetchPolicy: "no-cache",
+  });
+  console.log("data", data);
+  return (
+    <div>
+      Project
+      <div>
+        Here is my project.{" "}
+        {data.projects.map((project: IProject) => {
+          return (
+            <div key={project.id}>
+              {project.project_name}
+              {project.description}
+              {project.url_link}
+              {project.github_url_link}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
