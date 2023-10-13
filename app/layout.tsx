6ad17fec41278244911ae75a5afbc6029c5ac6eb
etan094 <etan094@gmail.com>
2023-10-12 17:43:55 -0700

    Fixed viewport height.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} py-32 px-5 md:px-36`}
+        className={`${inter.className} py-32 px-5 md:px-36 h-screen`}
         data-theme="light"
       >
         <ApolloWrapper>
